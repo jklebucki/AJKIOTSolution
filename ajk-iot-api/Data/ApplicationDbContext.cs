26357df009be956cc6405ddcb1d2a40ca484ae98
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2024-03-01 22:31:43 +0100

    API Initial

---
+++ ajk-iot-api/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ajk_iot_api.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
