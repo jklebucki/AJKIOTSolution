eada23313c06d042ba270aca5c0aaafbc2eae381
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2024-03-03 22:42:02 +0100

    Mobile & Web Init

---
+++ ajk-iot-web/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ajk_iot_web.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
