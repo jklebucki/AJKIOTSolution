e126d187c0582b0dcb28a51c237882bc676b4310
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2024-03-04 18:23:37 +0100

    WebApp init

---
+++ ajk-iot-web/Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,20 @@
+using ajk_iot_web.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace ajk_iot_web.Components.Account
+{
+    internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
+    {
+        public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+        {
+            var user = await userManager.GetUserAsync(context.User);
+
+            if (user is null)
+            {
+                redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+            }
+
+            return user;
+        }
+    }
+}
---
+++ ajk-iot-web/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace ajk_iot_web.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}
