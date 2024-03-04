bb0a26621621f01f244062b2d0535480352ba910
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2024-03-04 19:39:09 +0100

    WEB & API renamed

--- WEB/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace WEB.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
-}
