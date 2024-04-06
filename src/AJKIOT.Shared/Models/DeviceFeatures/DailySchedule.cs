7bb49f24637521656e9dd88fed2d4b3c8c8099cf
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2024-04-07 00:53:46 +0200

    Schedule refactoring

--- src/AJKIOT.Shared/Models/DeviceFeatures/DailySchedule.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AJKIOT.Shared.Models.DeviceFeatures
-{
-    public class DailySchedule
-    {
-        public int DayNumber { get; set; }
-        public List<DailyScheduleEntry> Entries { get; set; } = new List<DailyScheduleEntry>();
-    }
-}
