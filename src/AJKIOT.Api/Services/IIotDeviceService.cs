a171ca475914327171226496140e7d421ab92ede
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2024-04-09 10:47:25 +0200

    device service

--- src/AJKIOT.Api/Services/IIotDeviceService.cs
+++ src/AJKIOT.Api/Services/IIotDeviceService.cs
@@ -1,7 +1,13 @@
-﻿namespace AJKIOT.Api.Services
+﻿using AJKIOT.Api.Data.Migrations;
+using AJKIOT.Shared.Models;
+
+namespace AJKIOT.Api.Services
 {
     public interface IIotDeviceService
     {
+        Task<ApiResponse<int>> AddDevice(IotDevice iotDevice);
+        Task<ApiResponse<bool>> DeleteDevice(IotDevice iotDevice);
+        Task<ApiResponse<IEnumerable<IotDevice>>> GetUserDevicesDevices(string userId);
 
     }
 }
