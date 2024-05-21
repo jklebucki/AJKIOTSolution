d0cbf236227844482c38a14d15650fa079e8087b
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2024-05-21 21:13:18 +0200

    WEB Device online

---
+++ src/AJKIOT.Shared/Models/IotDeviceStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AJKIOT.Shared.Models
+{
+    public class IotDeviceStatus
+    {
+        public int DeviceId { get; set; }
+        public bool IsOnline { get; set; }
+        public DateTime LastOnlineSignal { get; set; }
+    }
+}
