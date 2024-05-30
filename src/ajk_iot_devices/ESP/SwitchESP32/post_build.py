c9c160457056dd73ffda8249365f47f6394e0a7c
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2024-05-30 12:49:54 +0200

    Switch build script

---
+++ src/ajk_iot_devices/ESP/SwitchESP32/post_build.py
@@ -0,0 +1,26 @@
+import shutil
+import os
+
+from SCons.Script import DefaultEnvironment
+
+env = DefaultEnvironment()
+
+def after_build(source, target, env):
+    # Ścieżka do wynikowego pliku binarnego
+    firmware_path = os.path.join(env.subst("$BUILD_DIR"), "firmware.bin")
+    
+    # Docelowa ścieżka, gdzie plik ma być skopiowany
+    # Przykład: skopiowanie do trzech poziomów wyżej w folderze output
+    project_dir = env.subst("$PROJECT_DIR")
+    destination_dir = os.path.abspath(os.path.join(project_dir, '..', '..', '..', 'AJKIOT.Api', 'FirmwareFiles', 'Switch'))
+    destination_path = os.path.join(destination_dir, "FirmwareSwitch.bin")
+    
+    # Utwórz folder docelowy, jeśli nie istnieje
+    os.makedirs(os.path.dirname(destination_path), exist_ok=True)
+    
+    # Skopiuj plik
+    shutil.copy(firmware_path, destination_path)
+    print(f"Firmware copied to {destination_path}")
+
+# Dodaj akcję post-build
+env.AddPostAction("buildprog", after_build)
